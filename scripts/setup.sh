e5e9bbf5a62eb41e7d95c5e9bb585728713ee39c
Kevin Ji <kevin.ji@outlook.com>
2016-12-24 00:29:11 -0500

    setup: Slightly tweak comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,10 +14,10 @@ for folder in "${folders[@]}"; do
     done
 done
 
-# Symlink zprezto into ~/.zprezto.
+# Symlink Prezto into ~/.zprezto.
 ln -s "$dotfiles"/zsh/prezto "$HOME"/.zprezto
 
-# Symlink the various zsh config files.
+# Symlink the various zsh config files into ~.
 for file in "$dotfiles"/zsh/*; do
     if [[ -f "$file" ]]; then
         ln -s "$file" "$HOME"/."$(basename "$file")"
