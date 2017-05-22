a6cd96375695d7ab33b0724daae2803c98908fee
Kevin Ji <kevin.ji@outlook.com>
2017-05-22 01:09:31 -0700

    scripts/setup: Add color scheme, fix dir symlinks
    Avoids the creation of new symlinks in existing dirs.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,24 +6,28 @@ dotfiles="$(cd "$scripts"/.. && pwd)"
 echo "Symlinking files..."
 
 # Symlink every dotfile into ~.
-folders=(homebrew git tmux vim)
+dotfile_folders=(homebrew git tmux vim)
 
-for folder in "${folders[@]}"; do
+for folder in "${dotfile_folders[@]}"; do
     for file in "$dotfiles"/"$folder"/*; do
-        ln -s "$file" "$HOME"/."$(basename "$file")"
+        ln -sn "$file" "$HOME"/."$(basename "$file")"
     done
 done
 
-# Symlink pip into ~/.pip.
-ln -s "$dotfiles"/pip "$HOME"/.pip
+# Symlink every dot-FOLDER into ~/.FOLDER.
+copy_folders=(pip shell)
+
+for folder in "${copy_folders[@]}"; do
+    ln -sn "$dotfiles"/"$folder" "$HOME"/."$folder"
+done
 
 # Symlink Prezto into ~/.zprezto.
-ln -s "$dotfiles"/zsh/prezto "$HOME"/.zprezto
+ln -sn "$dotfiles"/zsh/prezto "$HOME"/.zprezto
 
 # Symlink Prezto's zsh config files into ~.
 for file in "$dotfiles"/zsh/prezto/runcoms/*; do
     if [[ "$(basename "$file")" != "README.md" ]]; then
-        ln -s "$file" "$HOME"/."$(basename "$file")"
+        ln -sn "$file" "$HOME"/."$(basename "$file")"
     fi
 done
 
@@ -31,12 +35,12 @@ done
 for file in "$dotfiles"/zsh/*; do
     if [[ -f "$file" ]]; then
         rm -f "$HOME"/."$(basename "$file")"
-        ln -s "$file" "$HOME"/."$(basename "$file")"
+        ln -sn "$file" "$HOME"/."$(basename "$file")"
     fi
 done
 
 # Clone Tmux Plugin Manager into ~/.tmux/plugins.
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
 echo "Done symlinking files."
 echo
