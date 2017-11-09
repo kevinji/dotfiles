c1aaa88a3ddd8373735370dda3801741b72a723f
Kevin Ji <kevin.ji@outlook.com>
2017-11-09 15:13:03 -0800

    scripts: Remove Linuxbrew, adjust `brew` command check

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -7,22 +7,3 @@ dotfiles="$(cd "$scripts"/.. && pwd)"
 
 # Run general scripts.
 "$dotfiles"/scripts/setup.sh
-
-# Linuxbrew
-echo "Installing Linuxbrew..."
-
-if ! type brew &> /dev/null; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-fi
-
-brew tap homebrew/bundle
-brew update &> /dev/null
-
-# Install from Brewfile.
-brew bundle --global
-
-brew linkapps &> /dev/null
-brew cleanup &> /dev/null
-
-echo "Done installing Linuxbrew."
-echo
