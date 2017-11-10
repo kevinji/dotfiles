1edb2a9bc254e9809c2ebf2f6b5e7fd1f4cfcec2
Kevin Ji <kevin.ji@outlook.com>
2017-11-10 00:16:22 -0800

    scripts/setup: Install vim-plug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -44,6 +44,10 @@ done
 # Clone Tmux Plugin Manager into ~/.tmux/plugins.
 git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm || true
 
+# Install vim-plug into ~/.vim/autoload.
+curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "Done symlinking files."
 echo
 echo "Set zsh as your login shell: chsh -s \$(which zsh)"
