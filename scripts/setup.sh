705b63131c293c700313f21425a9b63a9631f262
Kevin Ji <kevinji@berkeley.edu>
2018-02-07 19:44:45 -0800

    scripts/setup: Fix path expansion

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,7 @@ echo "Symlinking files..."
 dotfile_folders=(git homebrew spacemacs tmux vim)
 
 for folder in "${dotfile_folders[@]}"; do
-    for file in "$dotfiles/$folder/*"; do
+    for file in "$dotfiles/$folder"/*; do
         ln -sn "$file" "$HOME/.$(basename "$file")" || true
     done
 done
@@ -27,14 +27,14 @@ done
 ln -sn "$dotfiles/zsh/prezto" "$HOME/.zprezto" || true
 
 # Symlink Prezto's zsh config files into ~.
-for file in "$dotfiles/zsh/prezto/runcoms/*"; do
+for file in "$dotfiles/zsh/prezto/runcoms"/*; do
     if [[ "$(basename "$file")" != "README.md" ]]; then
         ln -sn "$file" "$HOME/.$(basename "$file")" || true
     fi
 done
 
 # Symlink the overridden zsh config files into ~.
-for file in "$dotfiles/zsh/*"; do
+for file in "$dotfiles/zsh"/*; do
     if [[ -f "$file" ]]; then
         rm -f "$HOME/.$(basename "$file")"
         ln -sn "$file" "$HOME/.$(basename "$file")" || true
