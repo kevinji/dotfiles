d8066afac2339b3475ef79c99264d72f2086c239
Kevin Ji <kevin.ji@outlook.com>
2018-12-14 08:46:00 -0500

    spacemacs: Use ivy--regex-fuzzy for counsel-find-file

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -583,7 +583,8 @@ before packages are loaded."
     (ivy-extra-directories nil "Remove `.' and `..' from directory lists.")
     (ivy-initial-inputs-alist nil "Remove the initial `^' in searches.")
     (ivy-re-builders-alist
-      '((read-file-name-internal . ivy--regex-fuzzy)
+      '((counsel-find-file . ivy--regex-fuzzy)
+        (read-file-name-internal . ivy--regex-fuzzy)
         (ivy-switch-buffer . ivy--regex-fuzzy)
         (t . spacemacs/ivy--regex-plus)))
 
