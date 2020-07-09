40dbf0d334a1a735bedc24f582ef9c04404953aa
Kevin Ji <kevin.ji@outlook.com>
2020-07-09 14:19:18 -0400

    spacemacs: Reset text size

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -249,7 +249,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font or prioritized list of fonts.
    dotspacemacs-default-font '("Source Code Pro"
-                               ;; :size 13
+                               :size 13
                                :weight normal
                                :width normal)
 
