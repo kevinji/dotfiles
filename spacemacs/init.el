97c24aad8231cec21d77996d4f3ee1f6b952f5bd
Kevin Ji <kevin.ji@outlook.com>
2022-06-01 15:01:34 -0700

    spacemacs: Add prettier

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -91,6 +91,7 @@ This function should only modify configuration layer settings."
 
      ;; Tools
      docker
+     prettier
      )
 
    ;; List of additional packages that will be installed without being wrapped
