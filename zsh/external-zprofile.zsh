84157f4e801cec1b65fd73f8e6a5edde91163058
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2022-09-13 19:40:33 -0700

    zsh: Add Solana

--- zsh/external-zprofile.zsh
+++ zsh/external-zprofile.zsh
@@ -37,3 +37,8 @@ fi
 if [[ -d "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin" ]]; then
   path+=("/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin")
 fi
+
+# Solana
+if [[ -d "$HOME/.local/share/solana/install/active_release/bin" ]]; then
+  path+=("$HOME/.local/share/solana/install/active_release/bin")
+fi
