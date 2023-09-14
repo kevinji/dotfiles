309144a59e524da20d3ba38b516d33f048d35892
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-09-13 20:39:54 -0700

    doom: Add copilot

--- doom/config.el
+++ doom/config.el
@@ -102,6 +102,14 @@
   (setq vertico-multiform-categories
     '((file (vertico-sort-function . sort-directories-first)))))
 
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+
 (after! rustic
   (setq rustic-format-trigger 'on-save))
 
