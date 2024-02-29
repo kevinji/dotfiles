f4cd7061425a2ed0dc692b4e198f0dddf6e27aac
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-02-29 15:25:56 -0800

    zsh: Add golang set-env

--- zsh/external-zprofile.zsh
+++ zsh/external-zprofile.zsh
@@ -14,6 +14,10 @@ elif [[ -f "$HOME/.asdf/asdf.sh" ]]; then
   fpath+=("$ASDF_DIR/completions")
 fi
 
+if [[ -f "$HOME/.asdf/plugins/golang/set-env.zsh" ]]; then
+  source "$HOME/.asdf/plugins/golang/set-env.zsh"
+fi
+
 # Opam
 if (( $+commands[opam] )); then
   eval "$(opam env)"
