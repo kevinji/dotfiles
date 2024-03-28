59473a5131d2f704e8175d8848a94cc3f85973aa
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-03-27 22:46:54 -0700

    zsh: Add cross_linux function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -10,3 +10,9 @@ update_vim() {
 reload_tmux() {
   tmux source-file "$HOME/.tmux.conf"
 }
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  cross_linux() {
+    cross "$@" --target aarch64-unknown-linux-gnu
+  }
+fi
