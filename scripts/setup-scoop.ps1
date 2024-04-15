7e7a882e0f230bb7a10b4540d608edaa9f559074
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-04-14 22:02:21 -0700

    scripts: Remove unused setup-scoop.ps1

--- scripts/setup-scoop.ps1
+++
@@ -1,8 +0,0 @@
-# Install Scoop
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-
-# Install packages
-scoop bucket add extras
-scoop install aria2 sudo
-scoop config aria2-warning-enabled false
-scoop install 7zip dark delta gcloud git mercurial mpv python ripgrep rustup zstd
