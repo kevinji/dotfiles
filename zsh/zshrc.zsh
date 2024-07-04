9510e491a79bfde5add476a4deae893059f0b69d
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-07-03 21:19:17 -0700

    zsh: Replace pure with powerlevel10k

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,7 +9,19 @@ fi
 
 DIR="$(dirname "$(readlink -f "${(%):-%x}")")"
 
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 source "$DIR/core.zsh"
+
+# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+if [[ -f "$DIR/p10k.zsh" ]]; then
+  source "$DIR/p10k.zsh"
+fi
 
 if [[ -f "$DIR/zshrc-early-local.zsh" ]]; then
   source "$DIR/zshrc-early-local.zsh"
