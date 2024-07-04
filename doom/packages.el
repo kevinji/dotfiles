f89e92af2978454ce08cf26f474ffa3190c84c31
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-07-03 23:50:20 -0700

    doom: Remove dist from copilot.el

--- doom/packages.el
+++ doom/packages.el
@@ -50,6 +50,6 @@
 ;(unpin! t)
 
 (package! copilot
-  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 (package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 (package! protobuf-mode)
