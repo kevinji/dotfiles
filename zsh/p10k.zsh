ef1f68fb22e73f99ba0c86a8006ad5c9b00587e1
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-08-06 23:26:52 -0700

    zsh/p10k: Delete mise config

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -50,7 +50,6 @@
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     # asdf                  # asdf version manager (https://github.com/asdf-vm/asdf)
-    mise                    # mise version manager (https://github.com/jdx/mise)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     anaconda                # conda environment (https://conda.io/)
     # pyenv                 # python environment (https://github.com/pyenv/pyenv)
@@ -1710,42 +1709,6 @@
   # If p10k is already loaded, reload configuration.
   # This works even with POWERLEVEL9K_DISABLE_HOT_RELOAD=true.
   (( ! $+functions[p10k] )) || p10k reload
-}
-
-() {
-  function prompt_mise() {
-    local plugins=("${(@f)$(mise ls --current 2>/dev/null | awk '!/\(symlink\)/ && $3!="~/.tool-versions" && $3!="~/.config/mise/config.toml" && $3!="~/dotfiles/mise/config.toml" {print $1, $2}')}")
-    local plugin
-    for plugin in ${(k)plugins}; do
-      local parts=("${(@s/ /)plugin}")
-      local tool=${(U)parts[1]}
-      local version=${parts[2]}
-      p10k segment -r -i "${tool}_ICON" -s $tool -t "$version"
-    done
-  }
-
-  # Colors
-  typeset -g POWERLEVEL9K_MISE_BACKGROUND=1
-
-  typeset -g POWERLEVEL9K_MISE_DOTNET_CORE_BACKGROUND=5
-  typeset -g POWERLEVEL9K_MISE_ELIXIR_BACKGROUND=5
-  typeset -g POWERLEVEL9K_MISE_ERLANG_BACKGROUND=1
-  typeset -g POWERLEVEL9K_MISE_FLUTTER_BACKGROUND=4
-  typeset -g POWERLEVEL9K_MISE_GOLANG_BACKGROUND=4
-  typeset -g POWERLEVEL9K_MISE_HASKELL_BACKGROUND=3
-  typeset -g POWERLEVEL9K_MISE_JAVA_BACKGROUND=7
-  typeset -g POWERLEVEL9K_MISE_JULIA_BACKGROUND=2
-  typeset -g POWERLEVEL9K_MISE_LUA_BACKGROUND=4
-  typeset -g POWERLEVEL9K_MISE_NODEJS_BACKGROUND=2
-  typeset -g POWERLEVEL9K_MISE_PERL_BACKGROUND=4
-  typeset -g POWERLEVEL9K_MISE_PHP_BACKGROUND=5
-  typeset -g POWERLEVEL9K_MISE_POSTGRES_BACKGROUND=6
-  typeset -g POWERLEVEL9K_MISE_PYTHON_BACKGROUND=4
-  typeset -g POWERLEVEL9K_MISE_RUBY_BACKGROUND=1
-  typeset -g POWERLEVEL9K_MISE_RUST_BACKGROUND=208
-
-  # Substitute the default asdf prompt element
-  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=("${POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS[@]/asdf/mise}")
 }
 
 # Tell `p10k configure` which file it should overwrite.
