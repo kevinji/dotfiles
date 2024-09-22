e151b2969de623e5db68b532af6acd380be7e920
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-09-22 11:33:53 -0700

    scripts: Add sign-mullvad-nodes

---
+++ scripts/sign-mullvad-nodes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+pubkey=$(tailscale lock status --json | jq -r '.PublicKey')
+tlpub="tlpub:${pubkey#nlpub:}"
+
+mullvad_locked_nodes=$(tailscale lock status | grep -E 'mullvad.*nodekey')
+
+echo "$mullvad_locked_nodes" | while IFS= read -r line; do
+  echo "$line"
+
+  if [[ "$line" =~ "nodekey:"[[:space:]]*(.*) ]]; then
+    matched_string="nodekey:${match[1]}"
+
+    tailscale lock sign "$matched_string" "$tlpub"
+  fi
+done
