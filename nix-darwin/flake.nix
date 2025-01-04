db073a200a4652f75ef319cd1bb7ed8dbc12a8e2
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2025-01-04 15:41:09 -0800

    nix-darwin: Manage brew bundle

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -8,42 +8,233 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, determinate, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+  outputs =
+    inputs@{
+      self,
+      determinate,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [ ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      programs.bash.enable = false;
-      programs.fish.enable = false;
-      programs.zsh.enable = false;
+          # Enable Homebrew support.
+          homebrew.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          homebrew.taps = [
+            "cargo-lambda/cargo-lambda"
+            "hashicorp/tap"
+          ];
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+          homebrew.brews = [
+            # macOS App Store
+            "mas"
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .
-    darwinConfigurations."Io" = nix-darwin.lib.darwinSystem {
-      modules =
-        [ configuration
+            # Shell essentials
+            "zsh"
+            "antidote"
+            "tmux"
+
+            # Replacements for system programs
+            "bash"
+            "coreutils"
+            "gawk"
+            "gnu-sed"
+            "grep"
+            "vim"
+
+            # Version control
+            "git"
+
+            # Git helpers
+            "gh"
+            "git-delta"
+            "git-filter-repo"
+            "git-lfs"
+
+            # Editors
+            "neovim"
+
+            # Languages and package managers
+            "protobuf"
+            "python"
+            "hashicorp/tap/terraform"
+
+            "mise" # Language-agnostic
+            "opam" # Manages OCaml
+            "rustup" # Manages Rust
+
+            "uv" # Python package installer
+            "ruff" # Python linter/formatter
+
+            "bazelisk" # Bazel version manager
+            "cargo-lambda" # AWS Lambda functions in Rust
+            "dnscontrol" # DNS configuration as code
+            "golangci-lint" # Golang linter
+            "rust-analyzer" # Rust LSP
+            "shellcheck" # Bash static analysis
+            "tflint" # Terraform linter
+
+            # Databases
+            "postgresql@17"
+            "sqlite"
+
+            # Shell niceties
+            "curlie"
+            "fzf"
+            "gnupg"
+            "htop"
+            "jq"
+            "ncdu"
+            "wget"
+
+            # Rust-based shell tools
+            "bat" # cat
+            "difftastic"
+            "eza" # ls
+            "hyperfine" # bench
+            "fd" # find
+            "ripgrep" # grep
+            "xsv"
+
+            # Containers
+            "dive"
+            "docker"
+            "docker-buildx"
+            "docker-compose"
+            "docker-credential-helper"
+            "skopeo"
+
+            # Kubernetes
+            "helm"
+            "k3d"
+
+            # External services
+            "magic-wormhole"
+
+            # Video
+            "ffmpeg"
+            "yt-dlp"
+
+            # Image manipulation
+            "pngcrush"
+            "pngquant"
+
+            # Documentation
+            "graphviz"
+            "pandoc"
+
+            # Cloud CLIs
+            "awscli"
+
+            # Fonts
+            "fontforge"
+          ];
+
+          homebrew.casks = [
+            # Programming tools
+            "emacs"
+            "ghostty"
+            "iterm2"
+            "orbstack"
+            "utm"
+
+            # Utilities
+            "alfred"
+            "appcleaner"
+            "bartender"
+            "karabiner-elements"
+            "monitorcontrol"
+            "rectangle"
+
+            # AI
+            "ollama"
+
+            # Browsers
+            "arc"
+            "firefox"
+            "google-chrome"
+
+            # Storage
+            "dropbox"
+
+            # Audio
+            "audio-hijack"
+            "focusrite-control"
+            "spotify"
+
+            # Applications
+            "1password"
+            "iina"
+            "notion"
+            "steam"
+            "transmission"
+
+            # Communication
+            "discord"
+            "obs"
+            "signal"
+            "zoom"
+
+            # Cloud CLIs
+            "google-cloud-sdk"
+          ];
+
+          homebrew.masApps = {
+            "Xcode" = 497799835;
+
+            "Affinity Photo 2" = 1616822987;
+            "DaisyDisk" = 411643860;
+            "DaVinci Resolve" = 571213070;
+            "Fantastical" = 975937182;
+            "Flighty" = 1358823008;
+            "Messenger" = 1480068668;
+            "Parcel" = 639968404;
+            "PDF Expert" = 1055273043;
+            "Pixelmator Pro" = 1289583905;
+            "Slack for Desktop" = 803453959;
+            "Tailscale" = 1475387142;
+            "Telegram Lite" = 946399090;
+            "The Unarchiver" = 425424353;
+            "Things 3" = 904280696;
+            "Windows App" = 1295203466;
+            "Wipr" = 1320666476;
+          };
+
+          # Enable alternative shell support in nix-darwin.
+          programs.bash.enable = false;
+          programs.fish.enable = false;
+          programs.zsh.enable = false;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .
+      darwinConfigurations."Io" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
           determinate.darwinModules.default
         ];
+      };
+
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
     };
-  };
 }
--- scripts/setup-mac.sh
+++ scripts/setup-mac.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 set -e
-
-# Navigate to the dotfiles/ dir.
-scripts="$(dirname "${BASH_SOURCE[0]}")"
-dotfiles="$(cd "$scripts/.." && pwd)"
 
 # Xcode CLT
 xcode-select --install || true
@@ -14,14 +10,6 @@ echo "Installing Homebrew..."
 if [[ ! -x "$(command -v brew)" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-brew tap homebrew/bundle
-brew update &> /dev/null
-
-# Install from Brewfile.
-brew bundle --file="$dotfiles/homebrew/Brewfile"
-
-brew cleanup &> /dev/null
 
 echo "Done installing Homebrew."
 echo
