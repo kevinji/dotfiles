765a517a07446b0fbd4ad489adc6033cbb114aa2
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2025-06-09 19:29:57 -0700

    scripts: Fix antidote bundle for Nix

--- scripts/run-antidote-bundle.sh
+++ scripts/run-antidote-bundle.sh
@@ -4,6 +4,11 @@ if [[ -f "$HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh" ]]; then
   source "$HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh"
 elif [[ -f ~/.config/zsh/.antidote/antidote.zsh ]]; then
   source ~/.config/zsh/.antidote/antidote.zsh
+elif (( $+commands[nix] )); then
+  source "$(nix path-info 'nixpkgs#antidote')/share/antidote/antidote.zsh"
+elif (( ! $+commands[antidote] )); then
+  echo "antidote not found!"
+  exit 1
 fi
 
 antidote bundle < ~/.config/zsh/.zsh_plugins.txt > ~/.config/zsh/.zsh_plugins.zsh
