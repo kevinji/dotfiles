6090d2fda5665726f138098229196138235cffea
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2025-12-02 22:52:22 -0800

    zsh: Add upload_ghostty_terminfo function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,6 +11,10 @@ reload_tmux() {
   tmux source-file "$HOME/.tmux.conf"
 }
 
+upload_ghostty_terminfo() {
+  infocmp -x xterm-ghostty | ssh "$1" -- tic -x -
+}
+
 if [[ "${OSTYPE-}" == darwin* ]]; then
   cross_linux() {
     cross "$@" --target aarch64-unknown-linux-gnu
